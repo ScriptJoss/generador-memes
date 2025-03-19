3f9d967852535a0046e6842f67143fe829c808ea
Jose <josesitocrack0429p@gmail.com>
2025-03-18 21:39:50 -0500

    🍃 Generador memes con flask

---
+++ README.md
@@ -0,0 +1,4 @@
+# Generador de Memes
+
+- Generador de memes con flask.
+- Error en index por cuestiones de HTML, pero la aplicacion funciona como debe 👍🏻.
--- main.py
+++ main.py
@@ -4,43 +4,48 @@ from flask import Flask, render_template, request, send_from_directory
 
 app = Flask(__name__)
 
+
 # Resultados del formulario
-@app.route('/', methods=['GET','POST'])
+@app.route("/", methods=["GET", "POST"])
 def index():
-    if request.method == 'POST':
+    if request.method == "POST":
         # obtener la imagen seleccionada
-        selected_image = request.form.get('image-selector')
+        selected_image = request.form.get("image-selector")
 
-        # Asignación #2. Recepción del texto
-        
-
+        # Asignación #2. Hecho
+        textTop = request.form.get("textTop")
+        textBottom = request.form.get("textBottom")
         # Assignment #3. Receiving the text's positioning
-       
 
-        # Asignación #3. Recepción del posicionamiento del texto
-        
+        textTopY = request.form.get("textTopY")
+        textBottomY = request.form.get("textBottomY")
 
-        return render_template('index.html', 
-                               # Visualización de la imagen seleccionada
-                               selected_image=selected_image, 
+        selectedColor = request.form.get("color-selector")
 
-                               # Asignación #2. Visualización del texto
-                               
-
-                               #  Asignación #3. Visualización del color
-                               
-                               
-                               # Asignación #3. Visualización de la posición del texto
+        # Asignación #3. Hecho ✅
 
 
-                               )
+        return render_template(
+            "index.html",
+            # Visualización de la imagen seleccionada
+            selected_image=selected_image,
+            # Asignación #2. Hecho
+            textTop=textTop,
+            textBottom=textBottom,
+            #  Asignación #3. Visualización del color
+            selectedColor=selectedColor,
+            # Asignación #3. Visualización de la posición del texto
+            textTopY=textTopY,
+            textBottomY=textBottomY,
+        )
     else:
         # Mostrar la primera imagen por defecto
-        return render_template('index.html', selected_image='logo.svg')
+        return render_template("index.html", selected_image="logo.svg")
 
 
-@app.route('/static/img/<path:path>')
+@app.route("/static/img/<path:path>")
 def serve_images(path):
-    return send_from_directory('static/img', path)
+    return send_from_directory("static/img", path)
+
 
 app.run(debug=True)
